bb9872f39a393dd8c5c5119abcef581b799597aa
Greg Allen <greg@firstandthird.com>
2017-02-25 10:31:55 -0800

    linting

--- routes/api/databases.js
+++ routes/api/databases.js
@@ -3,14 +3,13 @@ exports.getDatabases = {
   method: 'GET',
   path: '/api/databases',
   handler(request, reply) {
-    const db = request.server.mongo.db;
-    db.admin().listDatabases((err, data) => {
+    const mongodb = request.server.mongo.db;
+    mongodb.admin().listDatabases((err, data) => {
       if (err) {
         throw err;
       }
 
       const dbs = [];
-      
       data.databases.forEach((db) => {
         dbs.push(db.name);
       });
