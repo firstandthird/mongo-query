7b98e519a8598bd48b62e19999b5b6f8d5da5697
Eric Willis <eric@ecwillis.com>
2017-02-28 02:28:54 +0000

    Removing debugging code

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,6 @@ exports.index = {
   method: 'GET',
   path: '/_nowhere',
   handler(request, reply) {
-    const db = request.mongo.db;
-
-    db.collection('users').find({}).toArray((err, data) => {
-      if (err) {
-        return reply(err);
-      }
-
-      reply(data);
-    });
+    reply('Hello world');
   }
 };
